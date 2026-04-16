01e1b558cd8de85d3a0c7a69407415571b8c2de2
noumanhafeez <noumanhafeez095@gmail.com>
2026-04-16 23:14:28 +0500

    feat(rag): add basic RAG pipeline for question answering over vector store

---
+++ src/rag_pipeline.py
@@ -0,0 +1,27 @@
+from utils.logging_integeration import get_logger
+
+logger = get_logger("rag_pipeline", "logs/rag_pipeline.log")
+
+class RAGService:
+
+    def __init__(self, vector_store, embedding_service):
+        self.vector_store = vector_store
+        self.embedding_service = embedding_service
+
+    def answer_question(self, question: str):
+
+        logger.info(f"Received query: {question}")
+
+        query_embedding = self.embedding_service.get_embeddings([question])
+
+        docs = self.vector_store.search(query_embedding)
+
+        context = "\n".join(docs)
+
+        answer = self.generate_answer(question, context)
+
+        return answer
+
+    def generate_answer(self, question, context):
+        # placeholder (replace with LLM like OpenAI / Llama)
+        return f"Based on context: {context[:300]}..."
